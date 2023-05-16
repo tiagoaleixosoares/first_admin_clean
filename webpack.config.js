5d770c7d80c9e00268525b251e47cfc4b02c534d
Tiago Soares <tiago.sousasoares@edp.com>
2023-05-16 23:47:59 +0100

    asdasd

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const webpack = require('webpack');
+
 module.exports = {
 
-    externals: [
+    plugins: [
+        // Work around for Buffer is undefined:
+        // https://github.com/webpack/changelog-v5/issues/10
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],externals: [
         'dtrace-provider',
         'fs',
         'mv',
